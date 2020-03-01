714bedbd18545cdd8685c96dc443c070d6cde298
Sahil Silare <sahilsilare@gmail.com>
2020-03-01 09:47:41 +0530

    Added new files

---
+++ CompleteThePattern2.js
@@ -0,0 +1,11 @@
+function pattern(n){
+    var output="";
+      // Happy Coding ^_^
+      for(var i=1;i<=n;i++){
+        for(var k=n;k>=i;k--){
+          output += k;
+         }
+          if(i!=n)output += "\n";
+      }
+    return output;
+   }
