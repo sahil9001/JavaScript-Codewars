d979c2add0e293b965b77006289ed8991c0ee7e8
sahil9001 <sahilsilare@gmail.com>
2020-03-31 23:52:57 +0530

    Added test

--- HumanReadableDurationFormat.js
+++ HumanReadableDurationFormat.js
@@ -1,37 +1,24 @@
-function formatDuration(seconds) {
-    // Complete this function
-    function buildString(value, unit) {
-        if (value > 0) {
-            return value + ' ' + unit + (value > 1 ? 's' : '') + ', ';
-        }
-        return '';
-    }
-    var str = "";
-    if (seconds === 0) return 'now';
-    else {
-        // console.log(seconds);
-        var years = Math.floor(seconds / 31536000);
-        seconds = seconds % 31536000;
-        var days = Math.floor(seconds / 86400);
-        seconds = seconds % 86400;
-        var hours = Math.floor(seconds / 3600);
-        seconds = seconds % 3600;
-        var minutes = Math.floor(seconds / 60);
-        seconds = seconds % 60;
-        str += buildString(years, 'year');
-        str += buildString(days, 'day');
-        str += buildString(hours, 'hour');
-        str += buildString(minutes, 'minute');
-        str += buildString(seconds, 'second');
-        var idx = str.lastIndexOf(', ');
-        str = str.substring(0, idx);
-
-
-        idx = str.lastIndexOf(', ');
-        if (idx > 0) {
-            str = str.substring(0, idx) + ' and ' + str.substring(idx + 2);
-        }
-
-        return str;
-    }
-}
+function humanReadable(seconds) {
+    const minutes = seconds / 60
+    const realSeconds = Math.floor(seconds % 60)
+    const realMinutes = Math.floor(minutes % 60)
+    const realHour = Math.floor(minutes / 60)
+  
+    const readableHour =
+      String(realHour).length === 1 ? String(realHour).padStart(2, '0') : realHour
+  
+    const readableMinutes =
+      String(realMinutes).length === 1
+        ? String(realMinutes).padStart(2, '0')
+        : realMinutes
+  
+    const readableSeconds =
+      String(realSeconds).length === 1
+        ? String(realSeconds).padStart(2, '0')
+        : realSeconds
+  
+    return `${readableHour}:${readableMinutes}:${readableSeconds}`
+  }
+  
+  // Function Export
+  module.exports = humanReadable
