3d820b47806f7b3236fc6ee9c8058737efe35d4a
sahil9001 <sahilsilare@gmail.com>
2020-04-01 00:29:57 +0530

    Added more tests

--- CompleteThePattern2.js
+++ CompleteThePattern2.js
@@ -9,3 +9,4 @@ function pattern(n){
       }
     return output;
    }
+  module.exports = pattern;
--- FirstNonConsecutive.js
+++ FirstNonConsecutive.js
@@ -1,6 +1,9 @@
 function firstNonConsecutive (arr) {
+    if(arr!== null){
     for(var i=0;i<arr.length-1;i++){
         if(arr[i]+1 != arr[i+1]) return arr[i+1];
     }
     return null;
 }
+}
+module.exports = firstNonConsecutive;
